c416544b180f490bf1e865b72901a459fea9b162
Kramclam <kennethramclam@gmail.com>
2022-09-16 15:18:09 -0400

    AB#650 generateScreen function for GUI, also objects

---
+++ src/com/game/controller/ActionHandler.java
@@ -0,0 +1,13 @@
+package com.game.controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ActionHandler implements ActionListener{
+
+    public ActionHandler(){}
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
