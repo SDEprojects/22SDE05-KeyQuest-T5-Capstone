7dbe7e44885bd883605edf7c39b7668e37d221f5
henrywg <henrywg@amazon.com>
2022-09-15 22:41:40 -0700

    AB#650, added comments and game flow document.

---
+++ test/TextParserTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextParserTest {
+
+    @Test
+    public short read() {
+        String[] test = {"test",};
+        Assertions.assertEquals("test",read() );
+        return 0;
+    }
+}
