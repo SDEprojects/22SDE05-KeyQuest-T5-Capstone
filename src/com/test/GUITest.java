cedf62945ef7dda6c8ac97e954f5c6424b9bdea9
Nina W <103607735+woownina@users.noreply.github.com>
2022-09-20 11:55:01 -0500

    "GUI test"

--- src/com/test/GUITest.java
+++ src/com/test/GUITest.java
@@ -1,17 +1,28 @@
+import com.gui.GUIClient;
+import com.gui.GUI;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.testing.AssertJSwingTestCaseTemplate;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import javax.swing.*;
+import java.awt.*;
 
 class GUITest extends AssertJSwingTestCaseTemplate {
-
+   //create fixture to handle Frame or Dialog in setUp()
+    private FrameFixture window;
 
     @BeforeEach
-    void setUp() {
+    void setUp() { //initializes test fixtures, runs everytime before test method is executed
+        GUIClient frame = GuiActionRunner.execute(() -> new GUIClient());
+        window = new FrameFixture(robot(), );
+        window.show(); //shows test frame
     }
 
     @AfterEach
     void tearDown() {
+        window.cleanUp();
     }
 
     @Test
@@ -26,9 +37,11 @@ class GUITest extends AssertJSwingTestCaseTemplate {
 
     @Test
     void createObject_menu() {
+
     }
 
     @Test
     void setMessageText() {
+
     }
 }
