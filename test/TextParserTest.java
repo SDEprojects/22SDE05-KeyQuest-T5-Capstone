0a3845cdd16cdcdb093f6491566899a96a84f153
Kramclam <kennethramclam@gmail.com>
2022-09-20 13:25:08 -0400

    AB#650 Game initilzies all background and objects

--- test/TextParserTest.java
+++ test/TextParserTest.java
@@ -1,14 +1,12 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TextParserTest {
 
     @Test
     public short read() {
-        String[] test = {"test",};
-        Assertions.assertEquals("test",read() );
+        String[] test = {"testing2",};
+        Assertions.assertEquals("testing2",read() );
         return 0;
     }
 }
