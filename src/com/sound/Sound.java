7dba9eb534305860a1b8bcfd6813d69316f3310b
Nina W <103607735+woownina@users.noreply.github.com>
2022-09-21 15:54:59 -0500

    "Sound class and .wav files in Resources folder"

--- src/com/sound/Sound.java
+++ src/com/sound/Sound.java
@@ -1,16 +1,23 @@
 package com.sound;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.*;
 import java.net.URL;
 
 public class Sound {
+    JFrame frame;
+    JPanel panel;
 
     Clip clip;
     URL[] soundURL =new URL[10]; //stores sound files
 
     public Sound() {
+        frame = new JFrame("Audio");
+        panel = new JPanel();
+        frame.add(panel);
+        frame.setVisible(true);
+
         soundURL[0] = getClass().getResource("/Sound/arcade-game.wav");
         soundURL[1] = getClass().getResource("/Sound/audio001.wav");
         soundURL[2] = getClass().getResource("/Sound/fanfare2.wav");
@@ -46,9 +53,10 @@ public class Sound {
         sound.loop();
     }
 
-    public static void main(String[] args) {
 
-        playMusic(1);
+    public static void main(String[] args) {
+        new Sound();
+        playMusic(0);
 
     }
 }
