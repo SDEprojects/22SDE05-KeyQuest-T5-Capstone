9a1d84ef36f53f9a7b3ed6d7cb6cca91c115f8a1
henrywg <henrywg@amazon.com>
2022-09-21 23:54:45 -0700

    AB#650, added lose game page, updated EventHandler.

--- src/com/game/controller/EventHandler.java
+++ src/com/game/controller/EventHandler.java
@@ -1,14 +1,17 @@
 package com.game.controller;
 
 import com.game.model.Character;
+import com.game.model.Location;
 import com.gui.GUIClient;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 
 import static com.game.utility.JSONParser.getAllItems;
+import static com.game.utility.JSONParser.getStartingRoom;
 
 
 public class EventHandler {
@@ -17,6 +20,12 @@ public class EventHandler {
     private List<String> inventory = new ArrayList<>(); // Initialize the inventory slots.
 
     private Set<String> listOfItems = getAllItems(); // Initialize the items.
+    // Set up the game actions.
+    String currentLocation = getStartingRoom();
+    Location location = new Location(currentLocation);
+    String[] listNextLocations = location.getDirections();
+    String[] characters = location.getCharacter();
+    // Set up game actions ends.
 
     public EventHandler(GUIClient guiClient) {
         this.guiClient = guiClient;
@@ -27,48 +36,66 @@ public class EventHandler {
         //TODO actionValue from ActionHandler when the label type is Jbutton;
         // When actionValue == click or do.
 
-        if (lableID == "foyer") {
-            // TODO Generate game page *Foyer*. Then go to Foyer.
-            //guiClient.getGui().generateScreen(2);
-            //guiClient.getGui().setupRoom();
+        if (actionValue.equals("foyer")) {
+            System.out.println("Create a new page of Foyer");
+            guiClient.getGui().generateScreen(2); // Set up the page with direction arrows.
+            // Inventory slots remain empty. Only action here is going to the kitchen.
+            guiClient.getGui().getMessageText().setText("You don't see anything valuable here.\nYou can go to " + Arrays.toString(listNextLocations));
+        } else if (actionValue.equals("kitchen")) {
+            System.out.println("Create a new page of Kitchen");
+            guiClient.getGui().generateScreen(3);
+            guiClient.getGui().getMessageText().setText("You are in the Kitchen, there is a drumstick and cucumber.\n You can go to " + Arrays.toString(listNextLocations));
+            if (actionValue.equals("drumstick")){
+                inventory.add("drumstick");
+                guiClient.getGui().getMessageText().setText("Drumstick is added to inventory");
+            } else if (actionValue.equals("cucumber")) {
+                inventory.add("cucumber");
+                guiClient.getGui().getMessageText().setText("Cucumber is added to inventory");
+            }
+            // TODO move items to inventory slots.
+        } else if (actionValue.equals("loft")) {
+            guiClient.getGui().generateScreen(4);
+            // Don't generate until user throw the item to the dog.
 
-        } else if (lableID.equals("foyer")) {
-            // TODO Generate game page *Kitchen*. Then go to Kitchen.
-            System.out.println("I AM HEERRRRE");
-        } else if (actionValue.equals("new game")) {
-            // TODO Generate game page *Loft*. Then go to Loft.
-            guiClient.getGui().generateScreen(1);
-            //gui.getMessageText().setText("Click on the items to see what you can do with them.");
-        } else if (lableID == "garage") {
-            // TODO Generate game page *Garage*. Then go to Garage.
-        } else if (lableID == "lounge") {
+            guiClient.getGui().getMessageText().setText("An alert dog watching out for lurking intruders.");
+            Character dog = new Character("dog");
+            // If click on the dog, means talk to the dag, if click the item, means throw.
+            if (actionValue.equals("dog")) {
+                guiClient.getGui().getMessageText().setText(String.valueOf(dog.getSpeech()));
+            } else if (actionValue.equals("drumstick")) {
+                //TODO populate direction arrows.
+                guiClient.getGui().getMessageText().setText("You distracted dog, you can go to Garage, Kitchen or Lounge.");
+            }
+
+        } else if (actionValue.equals("garage")) {
+            guiClient.getGui().generateScreen(5);
+            guiClient.getGui().getMessageText().setText("You are in the Garage. There are a lot of shiny items in here!");
+
+        } else if (actionValue.equals("lounge")) {
             // TODO Generate game page *Lounge*. Then go to Lounge.
-        } else if (lableID == "bathroom") {
+        } else if (actionValue.equals("bathroom")) {
             // TODO Generate game page *Bathroom*. Then go to Bathroom.
-        } else if (lableID == "closet") {
+        } else if (actionValue.equals("closet")) {
             // TODO Generate game page *Closet*. Then go to Closet.
-        } else if (lableID == "garden") {
+        } else if (actionValue.equals("garden")) {
             // TODO Do check if the player has the key in the inventory, if so, generate winning page.
-        } else if (lableID == "help") {
+        } else if (actionValue.equals("help")) {
             guiClient.getGui().getMessageText().setText("Click on the items to see what you can do with them.");
-        } else if (lableID == "btn_2") {
+        } else if (actionValue.equals("btn_2")) {
             // TODO Load game, future feature.
-        } else if (lableID == "btn_3") {
+        } else if (actionValue.equals("btn_3")) {
             // TODO Settings, future feature.
-        } else if (lableID == "btn_4"){
-            guiClient.getGui().getMessageText().setText("New Game clicked");
+        } else if (actionValue.equals("btn_4")) {
             guiClient.getGui().getMessageText().setText("Click 'help' to get available commands, right click items to see what you can do with them.");
-        } else if (lableID == "obj_1" || lableID == "obj_2" || lableID == "obj_3" || lableID == "obj_4") {
+        } else if (actionValue.equals("obj_1") || actionValue.equals("obj_2") || actionValue.equals("obj_3") || actionValue.equals("obj_4")) {
             // TODO If the item at the first location, add to inventory, else if lounge or loft, distract dog or cat. else text "no need to use the item here".
 
-        } else if (lableID == "obj_5") {
+        } else if (actionValue.equals("obj_5")) {
             // TODO the key. add to inventory.
-        } else if (lableID == "ch_1" || lableID == "ch_2") {
+        } else if (actionValue.equals("ch_1") || actionValue.equals("ch_2")) {
             // TODO Talk to dog or cat.
         }
     }
-
-
 
 
 }
